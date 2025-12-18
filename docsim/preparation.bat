8e71501a202d1e6d52b0ff790af9e32d28db8f70
joshuacline <114229084+joshuacline@users.noreply.github.com>
2025-12-18 05:09:18 -0700

    Create preparation.bat

---
+++ docsim/preparation.bat
@@ -0,0 +1,6 @@
+@echo off&chcp 65001>nul
+echo.Preparation for application stage begin
+echo.Generating file header
+echo.itchy.ou>application.txt
+echo.👏🤡>application.txt
+echo.Unicode text file created.
